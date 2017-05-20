c8f3105dda30554fe983c1099d3b6b837cf85af6
nvonbulow <nick@nvonbulow.com>
2017-05-20 14:32:52 -0400

    Various webpack optimizations

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,6 +6,10 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var StatsPlugin = require('stats-webpack-plugin');
+var FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+var WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+var ImageminPlugin = require('imagemin-webpack-plugin').default;
+var ImageminMozjpeg = require('imagemin-mozjpeg');
 
 module.exports = {
     // The entry file. All your app roots fromn here.
@@ -27,11 +31,6 @@ module.exports = {
         // copies files from one place to another
         new CopyWebpackPlugin([
             {
-                // copy all of the manifest icons into the assets folder
-                from: 'app/assets/images/manifest-icons',
-                to: 'assets'
-            },
-            {
                 // 404 error page handler
                 from: 'app/404.py'
             }
@@ -50,13 +49,34 @@ module.exports = {
             inject: 'body',
             filename: '404.html',
         }),
+        new FaviconsWebpackPlugin({
+            logo: './app/assets/images/logo/C.png',
+            prefix: 'assets/',
+            persistentCache: true,
+            inject: true,
+            background: '#f2f2f2',
+            title: 'Communicode',
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: false,
+                coast: false,
+                favicons: true,
+                firefox: false,
+                opengraph: false,
+                twitter: true,
+                yandex: false,
+                windows: true
+            }
+        }),
         // extracts the css from the js files and puts them on a separate .css file. this is for
         // performance and is used in prod environments. Styles load faster on their own .css
         // file as they dont have to wait for the JS to load.
         new ExtractTextPlugin('assets/[name]-[hash].min.css'),
         // handles uglifying js
         new webpack.optimize.UglifyJsPlugin({
-            compressor: {
+            compress: {
+                dead_code: true,
                 warnings: false,
                 screw_ie8: true,
             },
@@ -70,6 +90,28 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
+        new ImageminPlugin({
+            optipng: {
+                optimizationLevel: 3
+            },
+            gifsicle: {
+                optimizationLevel: 3
+            },
+            jpegtran: null,
+            svgo: {},
+            plugins: [
+                ImageminMozjpeg({
+                    quality: 90,
+                    progressive: true
+                })
+            ]
+
+        }),
+        /*
+        new WebpackCleanupPlugin( {
+            exclude: ['assets/icons/*']
+        })
+        */
     ],
 
     // ESLint options
